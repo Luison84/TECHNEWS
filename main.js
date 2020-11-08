28f022b8d705ad735b763dd3f6330ece12ebfe83
Luison84 <46362305+Luison84@users.noreply.github.com>
2020-11-08 00:45:17 -0500

    Revert "update_v1"
    This reverts commit 5a06c576220d3a17aaf3a459bcd7b2c8ac40177e.

--- main.js
+++ main.js
@@ -1,36 +1,4 @@
-window.addEventListener('load',function(){
-    new Glider(document.querySelector('.carousel__lista'),{
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: '.carousel__indicadores',
-        arrows: {
-            prev: '.carousel__anterior',
-            next: '.carousel__siguiente'
-        },
-        responsive: [
-          {
-            // screens greater than >= 800px
-            breakpoint: 800,
-            settings: {
-              // Set to `auto` and provide item width to adjust to viewport
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              itemWidth: 150,
-              duration: 0.25
-            }
-          },{
-            // screens greater than >= 1024px
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 4,
-            }
-          }
-        ]
-    });
-});
-
+console.log('hola')
 document.querySelector('.menu-btn').addEventListener('click', function() {
         document.querySelector('.nav-menu').classList.toggle('show');
 });
@@ -39,4 +7,3 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
-
